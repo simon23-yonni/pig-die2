1b8185605379f2ffedcdaf0466ae635798f9b7b3
simonyonni <simonyonni94@gmail.com>
2019-04-10 10:52:48 +0300

    scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,77 +1,61 @@
 //Buisness logic
-var randomNumber = function() {
-  return Math.floor(Math.random() * 6) + 1;
-}
+  $(document).ready(function() {
 
-function Player(rollScore, turnScore, totalScore) {
-  this.rollScore = rollScore;
-  this.turnScore = turnScore;
-  this.totalScore = totalScore;
+function Player() {
+  this.rollScore = 0;
+  this.turnScore = 0;
+  this.totalScore = 0;
+  this.playerName;
 }
 Player.prototype.roll = function() {
-  var random = randomNumber();
-  return random;
+  this.rollScore = Math.floor(Math.random() * 6) + 1;;
+
 }
 
 $(document).ready(function() {
 
-      var rollScore;
-      var turnScore;
-      var totalScore;
+var player1= new Player() ;
+var player2= new Player() ;
 
 
-      var player1 = new Player(rollScore = 0, turnScore = 0, totalScore = 0);
-      var player2 = new Player(rollScore = 0, turnScore = 0, totalScore = 0);
 
-      newPlayer.rollone = function() {
-        if (newPlayer.rollScore === 1) {
+
+      Player.prototype.score = function() {
+        if (this.rollScore == 1) {
           this.turnScore = 0;
           alert("Sorry " + this.playerName + ", you rolled a 1! time's up!")
         } else {
-          this.turnscore += this.roll;
+          this.turnscore += this.rollScore;
         }
       }
-      newPlayer.holdone = function() {
-        this.turnscore += this.rollScore;
-        this.totalScore = 0;
+      Player.holdone = function() {
+
+        this.totalScore =this.turnscore + this.rollScore;
         alert(this.playerName + ", your turn is over!");
       }
 
-      newPlayer.winner = function() {
-        if ((player1.totalScore + player1.turnScore) >= 100) {
-          alert("PLAYER ONE IS THE WINNER");
+    Player.winner = function() {
+        if (this.totalScore >= 100) {
+          alert(this.playerName, 'winner');
         }
       }
-      var player1 = new Player(rollScore = 0, turnScore = 0, totalScore = 0);
-      var player2 = new Player(rollScore = 0, turnScore = 0, totalScore = 0);
-      newPlayer.newGame = function() {
-          this.rollscore = 0;
-          this.tempscore = 0;
-          this.totalScore = 0;
-          this.playerName = "";
 
-          var cleaValues = function() {
-            $(".player1Name").val("");
-            $(".player2Name").val("");
-          }
+
+
+
           //user interface
           // User Interface
-          $(document).ready(function() {
+
 
                 $("button#start").click(function(event) {
                       player1 = new Player(true);
                       player2 = new Player(false);
-                      $(".player-console").show();
-                      $(".start-menu").hide();
 
-                      var player1Name = $(".player1Name").val();
-                      $("#player1Name").text(player1Name);
+                      $("#player1-roll").click(function(){
+                        player1.roll();
+                        $("#dice-roll-1").text(this.rollScore)
+                      })
 
-                      var player2Name = $(".player2Name").val();
-                      $("#player2Name").text(player2Name);
-
-                      player1.playerName = player1Name;
-                      player2.playerName = player2Name;
-
-
-                      
+})
+})
+})
